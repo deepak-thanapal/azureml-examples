795dae15520984d368d55fe95663158fc04fb6fe
deepak-thanapal <118607978+deepak-thanapal@users.noreply.github.com>
2022-11-19 17:27:59 +0530

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -27,7 +27,7 @@ az extension add -n ml -y
 ## For backward compatibility - running on old subscription
 # <set_variables>
 GROUP="azureml-examples"
-LOCATION="eastus"
+LOCATION="eastus2"
 WORKSPACE="main"
 # </set_variables>
 
